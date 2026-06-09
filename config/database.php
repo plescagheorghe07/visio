18c866e2c9cfebfbc8c6e9dd85ef35136bda9d93
plescagheorghe07 <155374122+plescagheorghe07@users.noreply.github.com>
2026-06-09 19:58:57 +0300

    Change database host and credentials to localhost

--- config/database.php
+++ config/database.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'host'    => 'sql110.infinityfree.com',
+    'host'    => 'localhost',
     'dbname'  => 'if0_42056622_visio',
     'charset' => 'utf8mb4',
-    'user'    => 'if0_42056622',
-    'pass'    => 'VWXpCYIQlm8O',
+    'user'    => 'root',
+    'pass'    => 'root',
 ];
